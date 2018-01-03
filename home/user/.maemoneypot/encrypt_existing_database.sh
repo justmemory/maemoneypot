4c545c55454971fe5354947f0d1441ef95700adb
Balázs Kaholek <35042909+justmemory@users.noreply.github.com>
2018-01-03 22:46:06 +0100

    Encrypt already existing database

--- home/user/.maemoneypot/encrypt_existing_database.sh
+++ home/user/.maemoneypot/encrypt_existing_database.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
 
+if [ -f "/home/user/.maemoneypot/.maemoneypotdb" ]; then
+    mv /home/user/.maemoneypot/.maemoneypotdb /home/user/.maemoneypot/.maemoneypotdb.bak
+fi
+
+sqlite3 -batch /home/user/.maemoneypot/.maemoneypotdb.bak <<"EOF"
+ATTACH DATABASE '/home/user/.maemoneypot/.maemoneypotdb' AS db KEY 'temporary_key';
+CREATE TABLE db.Expenses(Id integer primary key, Date date, Expense int, Category varchar, Tag varchar);
+CREATE TABLE db.Incomes(Id integer primary key, Date date, Income int, Category varchar);
+CREATE TABLE db.Savings(Id integer primary key, Date date, Saving int, Category varchar);
+INSERT INTO db.Expenses SELECT * FROM Expenses;
+INSERT INTO db.Incomes SELECT * FROM Incomes;
+INSERT INTO db.Savings SELECT * FROM Savings;
+DETACH DATABASE db;
+EOF
